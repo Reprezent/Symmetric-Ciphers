ee70ff573455e79ca96288942a11e4c85bd13155
J.T. Liso <gliso@hydra23.eecs.utk.edu>
2017-09-20 16:03:09 -0400

    setup command line parser. have not test yet, but it compiles

--- CommandLineArgParser.java
+++ CommandLineArgParser.java
@@ -8,54 +8,87 @@ class CommandLineArgParser
 {
     public CommandLineArgParser(String[] cmdopts)
     {
+		keyFile = null;
+		inputFile = null;
+		outputFile = null;
+		ivFile =  null;
 
+		int i;
+
+		//parsing list of command options
+		for(i = 0; i < cmdopts.length; i++){
+			switch(cmdopts[i]){
+				case "-k":
+					keyFile = cmdopts[i++];
+					break;
+
+				case "-i":
+					inputFile = cmdopts[i++];
+					break;
+				
+				case "-o":
+					outputFile = cmdopts[i++];
+					break;
+
+				case "-v":
+					ivFile = cmdopts[i++];
+					break;
+
+				default:
+					System.err.println("Ignoring command line argument " + cmdopts[i]);
+					break;
+			}
+		}
 
     }
 
     public boolean hasKeyFile()
     {
-
+		return keyFile != null;
     }
 
-    public String keyFile()
+    public String getKeyFile()
     {
-    
-
+		return keyFile;
     }
 
     public boolean hasInputFile()
     {
-
+		return inputFile != null;
     }
 
-    public String inputFile()
+    public String getInputFile()
     {
-
-    }
-
-
-    public boolean outputFile()
-    {
-
+		return inputFile;
     }
 
 
-    public File outputFIle()
+    public boolean hasoutputFile()
     {
+		return outputFile != null;
+    }
 
 
+    public String getOutputFile()
+    {
+		return outputFile;
     }
 
     public boolean hasIVFile()
     {
-
+		return ivFile != null;
     }
 
-    public String IVFile()
+    public String getIVFile()
     {
-
+		return ivFile;
     }
 
 
 
     private String opts;
+	private String keyFile;
+	private String inputFile;
+	private String outputFile;
+	private String ivFile;
+}
