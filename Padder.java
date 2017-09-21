118b0238e81e97e584a9f81dfb21779c4f0b3c17
J.T. Liso <gliso@hydra23.eecs.utk.edu>
2017-09-20 20:39:01 -0400

    wrote a rough padder

--- Padder.java
+++ Padder.java
@@ -4,17 +4,34 @@
 //
 
 
+import java.util.Arrays;
+
 class Padder
 {
-    // Function Name: 
-    //
-    // Param: 
-    // Param: 
-    //
-    // Return: 
-    //
-    public static Pad(byte[] data, int blocksize)
-    {
+	private int padlength;
 
+    // Function Name: pad 
+    //
+    // Param: data containing bytes for encrypted data
+    // Param: blockize of the IV
+    // Return: padded data 
+    //
+    public byte[] pad(byte[] data, int blocksize)
+    {
+		padlength = blocksize%data.length;
+
+		// padding an extra blocksize if the padlength would be 0
+		if(padlength == 0)
+			padlength = blocksize;
+		
+		byte[] padded_data = Arrays.copyOf(data, data.length+padlength);
+
+		//padding the data with the padlength 
+		for(int i = data.length; i < padded_data.length; i++){
+			padded_data[i] = (byte)padlength;
+		}
+
+		return padded_data;
     }
+
 }
