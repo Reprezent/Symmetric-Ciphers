85a859d6bf9a0c15aca6ae3c17a6a1a174a2c9bc
J.T. Liso <gliso@hydra23.eecs.utk.edu>
2017-09-20 21:30:14 -0400

    started the flow of CBC

--- CBC.java
+++ CBC.java
@@ -1,17 +1,50 @@
+import java.util.Random;
+import javax.crypto.*;
+import javax.crypto.spec.*;
+import java.security.*;
+
 class CBC{
-	private String iv;
+	private byte[] iv;
 	private AES aes;
 
-	public CBC(String i, AES a){
+	public CBC(byte[] i, AES a){
 		iv = i;
 		aes = a;
 	}
 
-	public String encrypt(String message){
+	public byte[] encrypt(byte[] msg){
+		byte[] encrypted = new byte[msg.length];
+		byte[] cipher = iv;
 
+		//block-chaining
+		for(int i = 0; i < msg.length; i+=iv.length){
+			byte[] seed = new byte[iv.length];
+
+			//XORing each byte in the message and current cipher
+			for(int j = 0; j < seed.length; j++){
+				seed[j] = (byte)(msg[i] ^ cipher[j]);
+			}
+
+			//putting the seed through the PSRF
+			//something = PSRF(seed);	
+
+			//copying the array over to encrypted array
+		//	for(int j = i; j < seed.length; j++)
+				//encrypted[j] = something;
+		}
+		
+
+		//padding after it has been encrypted
+		Padder pad = new Padder();
+		return pad.pad(encrypted, iv.length);
 	}
 
-	public String decrypt(String message){
+	public byte[] decrypt(byte[] msg){
+		byte[] decrypted =  new byte[msg.length];
+		
+		//unpad
 
+
+		return decrypted;
 	}
 }
