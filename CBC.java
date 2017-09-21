4df3e95e8ab5c84462c6e03aadc4704c65527372
J.T. Liso <gliso@hydra23.eecs.utk.edu>
2017-09-20 20:06:35 -0400

    skeleton for CBC and CTR

---
+++ CBC.java
@@ -0,0 +1,17 @@
+class CBC{
+	private String iv;
+	private AES aes;
+
+	public CBC(String i, AES a){
+		iv = i;
+		aes = a;
+	}
+
+	public String encrypt(String message){
+
+	}
+
+	public String decrypt(String message){
+
+	}
+}
---
+++ CTR.java
@@ -0,0 +1,17 @@
+class CTR{
+	private String iv;
+	private AES aes;
+
+	public CTR(String i, AES a){
+		iv = i;
+		aes = a;
+	}
+
+	public String encrypt(String message){
+
+	}
+
+	public String decrypt(String message){
+
+	}
+}
